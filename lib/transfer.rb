29e623fe08369d07e6827e957e6bc690446be7d8
natashabhuta <natasha.bhuta@gmail.com>
2018-09-01 23:44:46 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ def execute_transaction
   end 
 
 def reverse_transfer
-  if @status = "complete"
+  if @status =="complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
